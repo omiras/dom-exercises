45295dbcd904f5e43c77a29e60273f31fa358385
omiras <oscar.miras@gmail.com>
2023-10-17 22:24:23 +0200

    first commit

---
+++ ex1.js
@@ -0,0 +1,29 @@
+/**
+ * Completa cada ejercicio. Solo necesitas una línea de código JavaScript para cada ejercicio. BONUS: Haz el ejercicio sin modificar el ex1.html
+ */
+
+/**
+ * Cambia el título de la página a rojo usando un estilo en línea
+ */
+
+/**
+ * Cambia el tamaño del título de la página a 48px usando un estilo en línea
+ */
+
+/**
+ * Muestra por console.log el texto de la caja 2
+ */
+
+/**
+ * Cambia el color del borde del párrafo p.intro a verde usando un estilo en línea
+ */
+
+/**
+ * Elimina la clase "estilo-3" de la última caja.
+ * Ayuda: https://www.w3schools.com/howto/howto_js_remove_class.asp
+ */
+
+/**
+ * Añade la clase CSS "ocultar" al penúltimo párrafo
+ * Ayuda: https://simpledev.io/lesson/add-class-js/
+ */
